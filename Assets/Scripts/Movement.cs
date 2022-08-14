d74aa6ab62d214e346c7f559dafb44b610fbd9fd
Guillermo <guillearnautm@gmail.com>
2022-08-14 22:59:49 +0200

    player rotation and control changed

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -7,10 +7,13 @@ public class Movement : NetworkBehaviour {
 
 
 	public NetworkVariable<Vector3> n_Position = new NetworkVariable<Vector3>();
+	public NetworkVariable<Quaternion> n_Rotation = new NetworkVariable<Quaternion>();
+
 
 
 	Vector3 m_Input = Vector3.zero;
 	float m_Speed = 7f;
+	float m_RotationSpeed = 260; // degrees / second
 
 
 	Rigidbody rb;
@@ -36,14 +39,28 @@ public class Movement : NetworkBehaviour {
 
 
 	void ServerUpdate() {
-		rb.MovePosition(transform.position + m_Input * Time.deltaTime * m_Speed);
+		
+		// Movement
+		Vector3 forward = rb.transform.forward * Mathf.Max(0, m_Input.z);
+		rb.MovePosition(transform.position + forward * Time.deltaTime * m_Speed);
+
+		// Rotation
+		Vector3 rotation = new Vector3(0, m_Input.x, 0);
+		Vector3 deltaRotation = rotation * m_RotationSpeed * Time.deltaTime;
+		
+		rb.MoveRotation(rb.rotation * Quaternion.Euler(deltaRotation));
+		
+
 		n_Position.Value = transform.position;
+		n_Rotation.Value = rb.rotation;
+
+
 
 	}
 	void ClientUpdate() {
 		
 		transform.position = n_Position.Value;
-
+		transform.rotation = n_Rotation.Value;
 
 
 		Vector3 input = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
---
+++ Assets/Scripts/PlayerState.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+[Serializable]
+public class PlayerState {
+	public Vector3 position;
+	public Quaternion rotation;
+}
